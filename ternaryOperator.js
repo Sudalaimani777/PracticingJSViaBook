f07c0cea558cb293d44368c8e4a3cf2ab7043896
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-17 10:29:32 +0530

    Update ternaryOperator.js

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -1,19 +1,14 @@
-// const marks = 90;
+const marks = 90;
+console.log( marks > 30 ? "Passes" : "Failed" );
 
-// // console.log( marks > 30 ? "Passes" : "Failed" );
-
-// const result = marks < 20 ? "Work Hard 🤞" : marks < 40 ? "Keep Going 🎆 " : marks < 60 ? "Nice ♨️" : marks <= 70 ? "Suuuuuuperrrrr 🔥" : marks >= 90 ? "Massda 🌕" : "Rocking" ;
-
-
-// console.log(result);
+const results = marks < 20 ? "Work Hard 🤞" : marks < 40 ? "Keep Going 🎆 " : marks < 60 ? "Nice ♨️" : marks <= 70 ? "Suuuuuuperrrrr 🔥" : marks >= 90 ? "Massda 🌕" : "Rocking" ;
+console.log(results);
 
 
 
-// let i = "" ;
+let i = "" ;
 
-// for (i = 1 ; i <= 100 ; i++) {
-   
-//     let result = i % 3 === 0 ? `Fizz` : i % 5 === 0 ? `Buzz` : i % 15 === 0  ? `FizzBuzz` : i
-//     console.log(result);
-    
-// }
+for (i = 1 ; i <= 100 ; i++) { 
+   let result = i % 3 === 0 ? `Fizz` : i % 5 === 0 ? `Buzz` : i % 15 === 0  ? `FizzBuzz` : i
+  console.log(result);  
+ }
