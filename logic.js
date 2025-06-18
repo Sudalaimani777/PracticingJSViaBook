c350c8279dfa1824b711b83782d91334a4c4fff0
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-18 11:03:07 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,24 @@
+// export const stringsArray = ["Apple", "Watermelon", "BeetRoot"].sort();
+// // console.log(stringsArray);
+
+
+// if (stringsArray[0].length < stringsArray[1].length < stringsArray[2].length) {
+//     console.log(`Apple have the sortest length among all the array of ${stringsArray}`);
+// }
+// else if (stringsArray[1].length < stringsArray[0].length < stringsArray[2].length) {
+//     console.log(`Watermelon have the sortest length among all the array of ${stringsArray}`);
+// }
+// else if (stringsArray[2].length < stringsArray[0].length < stringsArray[1].length) {
+//     console.log(`Beetroot have the sortest length among all the array of ${stringsArray}`);
+// }
+// else {
+//     console.log(`Two or more strings are presented in the array`);
+    
+// }
+
+// export const filterMethod = stringsArray.filter( (stringsArray) => {
+
+//     console.log(`${stringsArray.length}`);
+    
+// })
+
