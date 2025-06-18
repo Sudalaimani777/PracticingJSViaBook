b2b95095190abac1f25f472bcf9fd43d02020ca8
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-18 11:09:21 +0530

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,31 +1,31 @@
 // //For Loop
-// for (let i = 0 ; i <= 10 ; i++) {
-//     console.log(i);
-// }
+ for (let i = 0 ; i <= 10 ; i++) {
+     console.log(i);
+ }
 
 //For Loop in the function
-// const userNames = (userName) => {
+ const userNames = (userName) => {
 
-//     for ( let i = 0 ; i <= userName.length ; i++) {
-//         console.log(userName[i]);
-//     }
+     for ( let i = 0 ; i <= userName.length ; i++) {
+         console.log(userName[i]);
+     }
 
-// }
-// userNames ("Sudalai");
+ }
+ userNames ("Sudalai");
 
 //Nested For Loop :-
-// for (let i = 0 ; i <= 10 ; i++) {
-//     for (let j = 0 ; j <= i ; j++) {
-//         let multiply = i * j ;
-//         console.log(`${i} * ${j} is ${multiply}`);
-//     }
-//     console.log(`----------------`);   
-// }
+ for (let i = 0 ; i <= 10 ; i++) {
+     for (let j = 0 ; j <= i ; j++) {
+         let multiply = i * j ;
+         console.log(`${i} * ${j} is ${multiply}`);
+     }
+     console.log(`----------------`);   
+}
 
-//Star Pattern
-// const symbol = "*";
+// Star Pattern
+ const symbol = "*";
 
-// for (let i= 0 ; i <=5 ; i++) {
-//     console.log(symbol.repeat(i));
+ for (let i= 0 ; i <=5 ; i++) {
+     console.log(symbol.repeat(i));
     
-// }
+}
