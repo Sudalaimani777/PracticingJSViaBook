8a590f883e62b31950ce027d6ab8b59e76db2dbf
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-18 11:04:36 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,24 +1,25 @@
-// export const stringsArray = ["Apple", "Watermelon", "BeetRoot"].sort();
-// // console.log(stringsArray);
+export const stringsArray = ["Apple", "Watermelon", "BeetRoot"].sort();
+console.log(stringsArray);
 
 
-// if (stringsArray[0].length < stringsArray[1].length < stringsArray[2].length) {
-//     console.log(`Apple have the sortest length among all the array of ${stringsArray}`);
-// }
-// else if (stringsArray[1].length < stringsArray[0].length < stringsArray[2].length) {
-//     console.log(`Watermelon have the sortest length among all the array of ${stringsArray}`);
-// }
-// else if (stringsArray[2].length < stringsArray[0].length < stringsArray[1].length) {
-//     console.log(`Beetroot have the sortest length among all the array of ${stringsArray}`);
-// }
-// else {
-//     console.log(`Two or more strings are presented in the array`);
+
+if (stringsArray[0].length < stringsArray[1].length < stringsArray[2].length) {
+    console.log(`Apple have the sortest length among all the array of ${stringsArray}`);
+}
+else if (stringsArray[1].length < stringsArray[0].length < stringsArray[2].length) {
+     console.log(`Watermelon have the sortest length among all the array of ${stringsArray}`);
+}
+else if (stringsArray[2].length < stringsArray[0].length < stringsArray[1].length) {
+     console.log(`Beetroot have the sortest length among all the array of ${stringsArray}`);
+}
+else {
+     console.log(`Two or more strings are presented in the array`);
     
-// }
+}
 
-// export const filterMethod = stringsArray.filter( (stringsArray) => {
+export const filterMethod = stringsArray.filter( (stringsArray) => {
 
-//     console.log(`${stringsArray.length}`);
+     console.log(`${stringsArray.length}`);
     
-// })
+})
 
