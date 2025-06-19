e8b17c365d33321b4ba5bed2343314d1e827d17c
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-19 14:17:41 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,9 +1,10 @@
-// export const heading = document.createElement("h1");
-// heading.innerText = "Hello World";
-// console.log(heading);
-// document.body.appendChild(heading);
-
-// export const para  = document.createElement("p");
-// para.innerText = "Simle";
-// document.body.appendChild(para)
-// console.log(para);
+//1
+export const heading = document.createElement("h1");
+heading.innerText = "Hello World";
+console.log(heading);
+document.body.appendChild(heading);
+//2
+export const para  = document.createElement("p");
+para.innerText = "Simle";
+document.body.appendChild(para)
+console.log(para);
