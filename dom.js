017c4b760610c8645c49244708f3ec5108ee8e37
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-19 14:16:54 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,9 @@
+// export const heading = document.createElement("h1");
+// heading.innerText = "Hello World";
+// console.log(heading);
+// document.body.appendChild(heading);
+
+// export const para  = document.createElement("p");
+// para.innerText = "Simle";
+// document.body.appendChild(para)
+// console.log(para);
