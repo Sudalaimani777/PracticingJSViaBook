751e9eab253301d8ea27b06aaa479b38e982897d
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-19 14:13:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { marks, result, i} from "./ternaryOperator.js";
 import { maths,physics, chemistry, biology, result, name } from "./logicalOperator.js";
 import { lastChar, reverse, arr, pushValue, popvalue } from "./function.js"
 import { dice, myFavAnimeList, calculateCircle, arrItem } from "./object.js";
+import { task, submitBtn, arr} from "./todolist.js";
 
 
 calculateCircle(30);
