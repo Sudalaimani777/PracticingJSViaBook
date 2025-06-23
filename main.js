2fc4117a846d897741fe7f3d33193a74161e0fd7
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-23 11:00:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,5 @@ import { dice, myFavAnimeList, calculateCircle, arrItem } from "./object.js";
 import { task, submitBtn, arr} from "./todolist.js";
 import { heading, para } from "./dom.js";
 import {headTag,keyBoardEvent, inputField, button, heading } from "./event.js";
+import { friuts, results, suits, values, deck, map, arrOfStr, strToUpper, spanners } from "./goingLoopyOverArrays.js";
 
