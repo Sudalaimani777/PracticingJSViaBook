e9e8684252f528000d5c5cb310087849dbea1a44
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-24 15:20:06 +0530

    Update string.js

--- string.js
+++ string.js
@@ -16,3 +16,8 @@ para.style.fontFamily = "arial "
 // It is used to check the character is present or not.It return the boolean value either true or false.
 console.log(mess.includes("e"));
 console.log(mess.includes("S"));
+
+//toUpperCase and toLowerCase :-
+//Used to conver the string into upperCase and lowerCase 
+console.log(mess.toLowerCase()); //sudalaimani
+console.log(mess.toUpperCase()); //SUDALAIMANI
