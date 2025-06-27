7f89254dd7d8ff13a36f675b8365e1323b93890f
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-27 11:41:20 +0530

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -8,35 +8,36 @@
                 Constructor we designed ourselves to produce objects for our own specific purposes.
 */ 
 
-// //Date Connstructor :-
-//  export const dateSnapshot = new Date()
-//  console.log(dateSnapshot);
+//Date Connstructor :-
+  export const dateSnapshot = new Date()
+  console.log(dateSnapshot);
 
-//  export const year = dateSnapshot.getFullYear();
-//  console.log(year);
+  export const year = dateSnapshot.getFullYear();
+  console.log(year);
  
-//  //Error Constructor :-
-//  export const errorMessage = (userNames) => {
-//     if(userNames) {
-//         console.log(userNames);
-//     }
-//     else{
-//         console.log(new Error(`Summa Try pannuda`));
-//     }
-//  }
-//  errorMessage("Hari");
-//  errorMessage();
+  //Error Constructor :-
+  export const errorMessage = (userNames) => {
+     if(userNames) {
+         console.log(userNames);
+     }
+     else{
+         console.log(new Error(`Summa Try pannuda`));
+     }
+  }
+  errorMessage("Hari");
+  errorMessage();
 
 
-// export function getStockData () {
-//     return {
-//         marketName : "QtechAI",
-//         sym : "QTA",
-//         price : (Math.random()*3).toFixed(2),
-//         time : new Date().toLocaleDateString()
-//     }
-// }
-// console.log(getStockData());
+ export function getStockData () {
+     return {
+         marketName : "QtechAI",
+         sym : "QTA",
+         price : (Math.random()*3).toFixed(2),
+         time : new Date().toLocaleDateString()
+     }
+ }
+ console.log(getStockData());
+
 //Challenge :-
 export const getStockData = () => {
     const stats = {
