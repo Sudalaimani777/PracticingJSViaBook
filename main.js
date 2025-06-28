81d6b08bd30901eeb10f52670e93a8e7c56244e5
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-28 13:06:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { stringsArray, filterMethod } from "./logic.js";
 import { marks, result, i} from "./ternaryOperator.js";
-import { maths,physics, chemistry, biology, result, name,playerGuess,correctAnswer,message, guess, selectItem, dreamHoliday } from "./logicalOperator.js";
+import { maths,physics, chemistry, biology, result, name,playerGuess,correctAnswer,message, guess, selectItem, dreamHoliday, user, hasValidEmail, response } from "./logicalOperator.js";
 import { lastChar, reverse, arr, pushValue, popvalue } from "./function.js"
 import { dice, myFavAnimeList, calculateCircle, arrItem } from "./object.js";
 import { task, submitBtn, arr} from "./todolist.js";
