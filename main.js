9fc5f929feb36909a372449f8823aa25ebaaf735
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-30 10:31:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { marks, result, i} from "./ternaryOperator.js";
 import { maths,physics, chemistry, biology, result, name,playerGuess,correctAnswer,message, guess, selectItem, dreamHoliday, user, hasValidEmail, response, isModerator, karma, hasEnoughKarma, canUpvote, canDelete} from "./logicalOperator.js";
 
 import { lastChar, reverse, arr, pushValue, popvalue, greet,calculateSum, findLargestNum, numRes,findSum,findSumRes, closure, outer } from "./function.js"
-import { dice, myFavAnimeList, calculateCircle, arrItem } from "./object.js";
+import { dice, myFavAnimeList, calculateCircle, arrItem, family, role } from "./object.js";
 import { task, submitBtn, arr} from "./todolist.js";
 import { heading, para } from "./dom.js";
 import {headTag,keyBoardEvent, inputField, button, heading } from "./event.js";
