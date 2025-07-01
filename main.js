fc633444357167119ff8e92e0c666780840039b4
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-07-01 10:33:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { stringsArray, filterMethod } from "./logic.js";
 import { marks, result, i} from "./ternaryOperator.js";
+
 import { maths,physics, chemistry, biology, result, name,playerGuess,correctAnswer,message, guess, selectItem, dreamHoliday, user, hasValidEmail, response, isModerator, karma, hasEnoughKarma, canUpvote, canDelete} from "./logicalOperator.js";
 
-import { lastChar, reverse, arr, pushValue, popvalue, greet,calculateSum, findLargestNum, numRes,findSum,findSumRes, closure, outer, capsName } from "./function.js"
+import { lastChar, reverse, arr, pushValue, popvalue, greet,calculateSum, findLargestNum, numRes,findSum,findSumRes, closure, outer, capsName, like , handlelike , countingDown , countdown , bill} from "./function.js"
+
 import { dice, myFavAnimeList, calculateCircle, arrItem, family, role, orgObject, deepCopy } from "./object.js";
 import { task, submitBtn, arr} from "./todolist.js";
 import { heading, para } from "./dom.js";
