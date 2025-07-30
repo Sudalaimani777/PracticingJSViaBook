a233534825b7465b54940636b8d2357087f434be
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-07-30 15:47:54 +0530

    Update arr.js

--- arr.js
+++ arr.js
@@ -8,6 +8,7 @@ export const checkPalindrome = () => {
 }
 checkPalindrome();
 
+
 //Spread in Arrays :- Spread -> Right Side , Rest -> Left Side.
 export const arrOne = [1, 2, 3, 4];
 export const arr22 = [3, 4, 5]
